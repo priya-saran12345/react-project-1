ea37bdb295e67d88a9bc13300b8c8c769b18fc12
priya-saran12345 <142259158+priya-saran12345@users.noreply.github.com>
2024-05-15 10:39:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import Main from './component/Main';
 import Navbar from './component/Navbar';
-import Playgame from './component/Playgame';
-import Startgame from './component/Startgame';
-import { Prev } from 'react-bootstrap/esm/PageItem';
 
 
 function App() {
-  const [play, setplay] = useState(false)
-  const changeplay=()=>{
-    setplay((prev)=>!prev)
-  }
   return (
     <>
         
